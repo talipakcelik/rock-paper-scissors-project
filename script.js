260f180b743625c8f214ba1f51e89ec008bc7fd7
Talip <talipakcelik@hacettepe.edu.tr>
2021-12-14 14:21:29 +0300

    02

--- script.js
+++ script.js
@@ -2,17 +2,61 @@
 
 const dizin = ["Taş", "Kağıt", "Makas"];
 
-// const computerPlay = function () {
-//   let random = dizin[Math.floor(Math.random() * dizin.length)];
-//   return console.log(random);
-// }
-
-computerPlay()
-
-
-const computerPlay = function () {
-  let random = dizin[Math.floor(Math.random() * dizin.length)];
-  return console.log(random);
+computerPlay = function () {
+  return dizin[Math.floor(Math.random() * dizin.length)];
 }
 
-computerPlay()
+//yöntem 1
+
+// let computerSelection = computerPlay();
+// const playerSelection = "taş";
+
+// playRound = function (playerSelection, computerSelection) {
+//   if (playerSelection === "taş" && computerSelection === "Kağıt")
+//     return "Kaybettin. Kağıt makası kaplar.";
+//   else if (playerSelection === "taş" && computerSelection === "Taş")
+//     return "Berabere.";
+//   else if (playerSelection === "taş" && computerSelection === "Makas")
+//     return "Kazandın. Taş makası kırar. "
+// }
+
+// function game() {
+//   computerSelection = computerPlay();
+//   console.log(playRound(playerSelection, computerSelection))
+//   computerSelection = computerPlay();
+//   console.log(playRound(playerSelection, computerSelection))
+//   computerSelection = computerPlay();
+//   console.log(playRound(playerSelection, computerSelection))
+//   computerSelection = computerPlay();
+//   console.log(playRound(playerSelection, computerSelection))
+// }
+
+// console.log(game())
+
+// yöntem 2
+
+
+
+
+playRound = function () {
+  const computerSelection = computerPlay();
+  const playerSelection = "taş";
+
+  if (playerSelection === "taş" && computerSelection === "Kağıt")
+    return "Kaybettin. Kağıt makası kaplar.";
+
+  else if (playerSelection === "taş" && computerSelection === "Taş")
+    return "Berabere.";
+
+  else if (playerSelection === "taş" && computerSelection === "Makas")
+    return "Kazandın. Taş makası kırar."
+}
+
+function game() {
+  console.log(playRound())
+  console.log(playRound())
+  console.log(playRound())
+}
+
+game()
+

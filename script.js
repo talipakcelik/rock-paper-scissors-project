aa7272e0029cafaecf36e8b63e548db7061c4bf1
Talip <talipakcelik@hacettepe.edu.tr>
2021-12-24 12:31:07 +0300

    03

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "strict use";
 
-const dizin = ["Taş", "Kağıt", "Makas"];
+const dizin = ["Rock", "Paper", "Scissors"];
 
 computerPlay = function () {
   return dizin[Math.floor(Math.random() * dizin.length)];
@@ -14,88 +14,90 @@ let computerScore = 0;
 playRound = function () {
   const computerSelection = computerPlay();
 
-  if (playerSelection === "taş" && computerSelection === "Kağıt") {
+  if (playerSelection === "rock" && computerSelection === "Paper") {
     if (playerScore < 5 && computerScore < 5) {
-      divs.textContent = "Kaybettin. Kağıt taşı kaplar.";
+      div1.textContent = "You lost. Rock is beaten by paper.";
       computerScore++;
-      return (div2.textContent = `Player Score = ${playerScore} - Computer Score ${computerScore}`);
+      return (div2.textContent = `Player Score = ${playerScore} - Computer Score = ${computerScore}`);
     } else if (playerScore >= 5)
-      return (div3.textContent = "Oyun bitti. Player kazandı.");
+      return (div3.textContent = "Game is over. Player won.");
     else if (computerScore >= 5)
-      return (div3.textContent = "Oyun bitti. Computer kazandı.");
-  } else if (playerSelection === "taş" && computerSelection === "Taş") {
+      return (div3.textContent = "Game is over. Computer won.");
+  } else if (playerSelection === "rock" && computerSelection === "Rock") {
     if (playerScore < 5 && computerScore < 5) {
-      divs.textContent = "Berabere.";
-      return (div2.textContent = `Player Score = ${playerScore} - Computer Score ${computerScore}`);
+      div1.textContent = "It's a tie!. Rock ties with rock";
+      return (div2.textContent = `Player Score = ${playerScore} - Computer Score = ${computerScore}`);
     } else if (playerScore >= 5)
-      return (div3.textContent = "Oyun bitti. Player kazandı.");
+      return (div3.textContent = "Game is over. Player won.");
     else if (computerScore >= 5)
-      return (div3.textContent = "Oyun bitti. Computer kazandı.");
-  } else if (playerSelection === "taş" && computerSelection === "Makas") {
+      return (div3.textContent = "Game is over. Computer won.");
+  } else if (playerSelection === "rock" && computerSelection === "Scissors") {
     if (playerScore < 5 && computerScore < 5) {
-      divs.textContent = "Kazandın. Taş makası kırar.";
+      div1.textContent = "You won! Rock beats scissors.";
       playerScore++;
-      return (div2.textContent = `Player Score = ${playerScore} - Computer Score ${computerScore}`);
+      return (div2.textContent = `Player Score = ${playerScore} - Computer Score = ${computerScore}`);
     } else if (playerScore >= 5)
-      return (div3.textContent = "Oyun bitti. Player kazandı.");
+      return (div3.textContent = "Game is over. Player won.");
     else if (computerScore >= 5)
-      return (div3.textContent = "Oyun bitti. Computer kazandı.");
-  } else if (playerSelection === "kağıt" && computerSelection === "Kağıt") {
+      return (div3.textContent = "Game is over. Computer won.");
+  } else if (playerSelection === "paper" && computerSelection === "Paper") {
     if (playerScore < 5 && computerScore < 5) {
-      divs.textContent = "Berabere.";
-      return (div2.textContent = `Player Score = ${playerScore} - Computer Score ${computerScore}`);
+      div1.textContent = "It's a tie! Paper ties with paper";
+      return (div2.textContent = `Player Score = ${playerScore} - Computer Score = ${computerScore}`);
     } else if (playerScore >= 5)
-      return (div3.textContent = "Oyun bitti. Player kazandı.");
+      return (div3.textContent = "Game is over. Player won.");
     else if (computerScore >= 5)
-      return (div3.textContent = "Oyun bitti. Computer kazandı.");
-  } else if (playerSelection === "kağıt" && computerSelection === "Taş") {
+      return (div3.textContent = "Game is over. Computer won.");
+  } else if (playerSelection === "paper" && computerSelection === "Rock") {
     if (playerScore < 5 && computerScore < 5) {
-      divs.textContent = "Kazandın. Kağıt taşı kaplar.";
+      div1.textContent = "You won! Paper beats rock.";
       playerScore++;
-      return (div2.textContent = `Player Score = ${playerScore} - Computer Score ${computerScore}`);
+      return (div2.textContent = `Player Score = ${playerScore} - Computer Score = ${computerScore}`);
     } else if (playerScore >= 5)
-      return (div3.textContent = "Oyun bitti. Player kazandı.");
+      return (div3.textContent = "Game is over. Player won.");
     else if (computerScore >= 5)
-      return (div3.textContent = "Oyun bitti. Computer kazandı.");
-  } else if (playerSelection === "kağıt" && computerSelection === "Makas") {
+      return (div3.textContent = "Game is over. Computer won.");
+  } else if (playerSelection === "paper" && computerSelection === "Scissors") {
     if (playerScore < 5 && computerScore < 5) {
-      divs.textContent = "Kaybettin. Makas kağıdı keser.";
+      div1.textContent = "You lost! Paper is beaten by scissors.";
       computerScore++;
-      return (div2.textContent = `Player Score = ${playerScore} - Computer Score ${computerScore}`);
+      return (div2.textContent = `Player Score = ${playerScore} - Computer Score = ${computerScore}`);
     } else if (playerScore >= 5)
-      return (div3.textContent = "Oyun bitti. Player kazandı.");
+      return (div3.textContent = "Game is over. Player won.");
     else if (computerScore >= 5)
-      return (div3.textContent = "Oyun bitti. Computer kazandı.");
-  } else if (playerSelection === "makas" && computerSelection === "Kağıt") {
+      return (div3.textContent = "Game is over. Computer won.");
+  } else if (playerSelection === "scissors" && computerSelection === "Paper") {
     if (playerScore < 5 && computerScore < 5) {
-      divs.textContent = "Kazandın. Makas kağıdı keser.";
+      div1.textContent = "You won! Scissors beats paper.";
       playerScore++;
-      return (div2.textContent = `Player Score = ${playerScore} - Computer Score ${computerScore}`);
+      return (div2.textContent = `Player Score = ${playerScore} - Computer Score = ${computerScore}`);
     } else if (playerScore >= 5)
-      return (div3.textContent = "Oyun bitti. Player kazandı.");
+      return (div3.textContent = "Game is over. Player won.");
     else if (computerScore >= 5)
-      return (div3.textContent = "Oyun bitti. Computer kazandı.");
-  } else if (playerSelection === "makas" && computerSelection === "Taş") {
+      return (div3.textContent = "Game is over. Computer won.");
+  } else if (playerSelection === "scissors" && computerSelection === "Rock") {
     if (playerScore < 5 && computerScore < 5) {
-      divs.textContent = "Kaybettin. Taş makası kırar.";
+      div1.textContent = "You lost! Scissors is beaten by rock.";
       computerScore++;
-      return (div2.textContent = `Player Score = ${playerScore} - Computer Score ${computerScore}`);
+      return (div2.textContent = `Player Score = ${playerScore} - Computer Score = ${computerScore}`);
     } else if (playerScore >= 5)
-      return (div3.textContent = "Oyun bitti. Player kazandı.");
+      return (div3.textContent = "Game is over. Player won.");
     else if (computerScore >= 5)
-      return (div3.textContent = "Oyun bitti. Computer kazandı.");
-  } else if (playerSelection === "makas" && computerSelection === "Makas") {
+      return (div3.textContent = "Game is over. Computer won.");
+  } else if (
+    playerSelection === "scissors" &&
+    computerSelection === "Scissors"
+  ) {
     if (playerScore < 5 && computerScore < 5) {
-      divs.textContent = "Berabere.";
-      return (div2.textContent = `Player Score = ${playerScore} - Computer Score ${computerScore}`);
+      div1.textContent = "It's a tie! Scissors ties with scissors";
+      return (div2.textContent = `Player Score = ${playerScore} - Computer Score = ${computerScore}`);
     } else if (playerScore >= 5)
-      return (div3.textContent = "Oyun bitti. Player kazandı.");
+      return (div3.textContent = "Game is over. Player won.");
     else if (computerScore >= 5)
-      return (div3.textContent = "Oyun bitti. Computer kazandı.");
+      return (div3.textContent = "Game is over. Computer won.");
   }
 };
 
-console.log(playerScore, computerScore, "dda");
 function game() {
   return playRound();
 }
@@ -104,13 +106,13 @@ function game() {
 
 const btn1 = document.createElement("button");
 btn1.classList.add("buton1");
-btn1.textContent = "Taş";
+btn1.textContent = "Rock";
 const btn2 = document.createElement("button");
 btn2.classList.add("buton2");
-btn2.textContent = "Kağıt";
+btn2.textContent = "Paper";
 const btn3 = document.createElement("button");
 btn3.classList.add("buton3");
-btn3.textContent = "Makas";
+btn3.textContent = "Scissors";
 
 document.body.appendChild(btn1);
 document.body.appendChild(btn2);
@@ -121,55 +123,21 @@ const butonAll = document.querySelectorAll("button");
 butonAll.forEach((button) =>
   button.addEventListener("click", function (e) {
     if (e.target.className === "buton1") {
-      playerSelection = "taş";
-      console.log("aaa");
+      playerSelection = "rock";
       return console.log(game());
     } else if (e.target.className === "buton2") {
-      playerSelection = "kağıt";
+      playerSelection = "paper";
       return console.log(game());
     } else if (e.target.className === "buton3") {
-      playerSelection = "makas";
+      playerSelection = "scissors";
       return console.log(game());
     }
   })
 );
 
-const divs = document.createElement("div");
-document.body.appendChild(divs);
+const div1 = document.createElement("div");
+document.body.appendChild(div1);
 const div2 = document.createElement("div");
 document.body.appendChild(div2);
 const div3 = document.createElement("div");
 document.body.appendChild(div3);
-// console.log(butonAll);
-
-// butonAll.addEventListener("click", function (e) {
-//   console.log(e);
-// }); hata aldık
-
-/********************************* */
-//yöntem 2
-
-// let computerSelection = computerPlay();
-// const playerSelection = "taş";
-
-// playRound = function (playerSelection, computerSelection) {
-//   if (playerSelection === "taş" && computerSelection === "Kağıt")
-//     return "Kaybettin. Kağıt makası kaplar.";
-//   else if (playerSelection === "taş" && computerSelection === "Taş")
-//     return "Berabere.";
-//   else if (playerSelection === "taş" && computerSelection === "Makas")
-//     return "Kazandın. Taş makası kırar. "
-// }
-
-// function game() {
-//   computerSelection = computerPlay();
-//   console.log(playRound(playerSelection, computerSelection))
-//   computerSelection = computerPlay();
-//   console.log(playRound(playerSelection, computerSelection))
-//   computerSelection = computerPlay();
-//   console.log(playRound(playerSelection, computerSelection))
-//   computerSelection = computerPlay();
-//   console.log(playRound(playerSelection, computerSelection))
-// }
-
-// console.log(game())
